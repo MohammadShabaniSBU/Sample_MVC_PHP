c3143afae1093dc2a444c13cb5d79e09c8563598
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-07 10:39:45 +0430

    sign up method completed

--- app/controller/AuthController.php
+++ app/controller/AuthController.php
@@ -2,7 +2,11 @@
 
 namespace app\controller;
 
+use app\core\Error;
 use app\core\Request;
+use app\core\Validation;
+use app\core\View;
+use app\models\User;
 
 class AuthController extends Controller {
 
@@ -19,7 +23,52 @@ class AuthController extends Controller {
     }
 
     public function signUp(Request $request) {
+        $request = $request->getParams();
 
+        Validation::make()->rules($this->signInRules())->data($request)->validate();
+
+        if (Error::getInstance()->hasError()) {
+            $this->render('signUp', ['title' => 'Sign Up']);
+            return;
+        }
+
+        User::Do()->insert($this->makeSignUpModelData($request))->execute();
+
+        header("Location: /signIn");
+
+
+    }
+
+    private function signInRules() {
+
+        return [
+            'firstname' => [
+                'alphabetic',
+                ['minLen', 4],
+                ['maxLen', 16],
+            ],
+            'lastname' => [
+                'alphabetic',
+                ['minLen', 4],
+                ['maxLen', 16],
+            ],
+            'email' => [
+                'email',
+            ],
+            'password' => [
+                'confirmation'
+            ]
+        ];
+    }
+
+    private function makeSignUpModelData($request) {
+        return [
+            'firstname' => $request['firstname'],
+            'lastname' => $request['lastname'],
+            'email' => $request['email'],
+            'password' => md5($request['password']),
+            'type' => 'normal',
+        ];
     }
 
 }
