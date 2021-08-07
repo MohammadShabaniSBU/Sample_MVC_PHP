7b4ffc280d65a8e7c02551745d4b5ee1ad0353d2
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-07 11:29:37 +0430

    Response class and setUserCookie method is added

--- app/core/Response.php
+++ app/core/Response.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace app\core;
+
+class Response {
+    
+    public static function setUserCookie(string $userId) {
+        setcookie('user_id', $userId, [
+            'path' => '/',
+            'expires' => time() + 86400,
+        ]);
+    }
+}
