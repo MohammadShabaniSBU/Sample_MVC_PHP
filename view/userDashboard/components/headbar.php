bc579c52e14dc66087ffc4090b254c1c56ba083d
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-09 03:54:06 +0430

    components is added

--- view/userDashboard/components/headbar.php
+++ view/userDashboard/components/headbar.php
@@ -1,1 +1,11 @@
-<?php
+<div class="d-flex justify-content-between align-items-center p-2">
+    <div class="fw-bold fs-4">
+        mohammad shabani
+    </div>
+    <div>
+        <a href="" data-bs-toggle="tooltip" data-bs-placement="top" title="Logout">
+            <i class="bi bi-x-circle fs-3 text-danger"></i>
+        </a>
+    </div>
+</div>
+<hr>
--- view/userDashboard/components/sidebar.php
+++ view/userDashboard/components/sidebar.php
@@ -1,1 +1,8 @@
-<?php
+<div class="col-2 h-100 border-end border-success pe-0">
+    <a href="/dashboard/files" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
+        Files
+    </a>
+    <a href="/dashboard/profile" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
+        Profile
+    </a>
+</div>
