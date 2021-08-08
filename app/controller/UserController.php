3480bddf4931a2d06c9fe7f3818301c2b89885aa
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-09 03:50:52 +0430

    showFiles and showProfile methods added

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -2,7 +2,14 @@
 
 namespace app\controller;
 
-class UserController extends Controller
-{
+class UserController extends Controller {
+    public function showFiles() {
 
+        $this->render('userDashboard/files', ['title' => 'Files']);
+    }
+
+    public function showProfile() {
+
+        $this->render('userDashboard/profile', ['title' => 'Profile']);
+    }
 }
