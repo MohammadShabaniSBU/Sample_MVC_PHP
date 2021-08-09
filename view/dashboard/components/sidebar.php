e0c5a46df5ac55a5048ced338bb40584bbbbfc1b
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-09 11:02:05 +0430

    userDashboard changed into dashboard

---
+++ view/dashboard/components/sidebar.php
@@ -0,0 +1,17 @@
+<div class="col-2 h-100 border-end border-success pe-0">
+    <a href="/dashboard/uploads" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
+        Uploaded Files
+    </a>
+    <a href="/dashboard/downloads" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
+        Downloaded Files
+    </a>
+    <a href="/dashboard/requests" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
+        Requested Files
+    </a>
+    <a href="/dashboard/users" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
+        Users Management
+    </a>
+    <a href="/dashboard/profile" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
+        Profile
+    </a>
+</div>
---
+++ view/dashboard/downloads.php
@@ -0,0 +1,107 @@
+
+<!-- Edit Modal -->
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editModal">Edit</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" class="form-floating">
+                    <div class="form-floating my-2">
+                        <input type="text" class="form-control" name='title' id='title' placeholder=''>
+                        <label for="title">Title</label>
+                    </div>
+                    <div class="form-floating my-2">
+                        <input type="text" class="form-control" name='price' id='price' placeholder=''>
+                        <label for="price">Price</label>
+                    </div>
+
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-success">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Delete Modal -->
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteModal">Delete</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div>
+                    Are you sure to delete this file?
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<div class="row m-0">
+    <div class="d-flex justify-content-center align-items-center h-100-vh">
+        <div class="col-10 h-80-vh border rounded-3 bg-light">
+            <div class="row h-100">
+                <?php include \app\core\App::$root . '/view/dashboard/components/sidebar.php';  ?>
+                <div class="col-10">
+                    <?php include \app\core\App::$root . '/view/dashboard/components/headbar.php';  ?>
+
+                    <div class='p-2'>
+                        <table class="table table-striped text-center">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Title</th>
+                                <th scope="col">Owner</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Download time</th>
+                                <th scope="col">Download</th>
+                                <th scope="col">Edit</th>
+                                <th scope="col">Delete</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                <td>25</td>
+                                <td>
+                                    <a href="">
+                                        <i class="bi bi-cloud-download fs-5 text-success"></i>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href=""  data-bs-toggle="modal" data-bs-target="#editModal">
+                                        <i class="bi bi-pencil fs-5 text-primary"></i>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                                        <i class="bi bi-trash fs-5 text-danger"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
