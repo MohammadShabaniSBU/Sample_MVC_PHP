ad0284558bd9dc5f75bf319f6c1a5b0fa4f99ed8
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-09 05:09:28 +0430

    downloaded and uploaded files got sepreated

--- view/userDashboard/components/sidebar.php
+++ view/userDashboard/components/sidebar.php
@@ -1,6 +1,9 @@
 <div class="col-2 h-100 border-end border-success pe-0">
-    <a href="/dashboard/files" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
-        Files
+    <a href="/dashboard/uploaded" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
+        Uploaded Files
+    </a>
+    <a href="/dashboard/downloaded" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
+        Downloaded Files
     </a>
     <a href="/dashboard/profile" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
         Profile
