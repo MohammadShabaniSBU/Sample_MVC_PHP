036aa8ba775fddab0b29d8e196f95eac973e9ace
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-09 11:04:20 +0430

    admin controller intialised

--- app/controller/AdminController.php
+++ app/controller/AdminController.php
@@ -2,7 +2,14 @@
 
 namespace app\controller;
 
-class AdminController extends Controller
-{
+class AdminController extends Controller {
+    public function showRequests() {
 
+        $this->render('dashboard/requests', ['title' => 'Requested Files']);
+    }
+
+    public function showUsers() {
+
+        $this->render('dashboard/users', ['title' => 'Users Management']);
+    }
 }
