f8e77dcfb21e3d39f5551e3017729089180f5e59
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-11 04:21:16 +0430

    settings added to sidebar

--- view/dashboard/components/sidebar.php
+++ view/dashboard/components/sidebar.php
@@ -11,6 +11,9 @@
     <a href="/dashboard/users" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
         Users Management
     </a>
+    <a href="/dashboard/settings" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
+        Settings
+    </a>
     <a href="/dashboard/profile" class="btn btn-outline-success border-0 border-bottom w-100 text-start rounded-0 fs-5">
         Profile
     </a>
