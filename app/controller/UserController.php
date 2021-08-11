1b888fb5408267c18038bfbd83cb721498cb022e
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-11 14:34:35 +0430

    upload stuff added

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -2,7 +2,14 @@
 
 namespace app\controller;
 
+use app\core\Error;
+use app\core\Redirect;
+use app\core\Request;
+use app\core\Validation;
+use app\models\File;
+
 class UserController extends Controller {
+
     public function showUploads() {
 
         $this->render('dashboard/uploads', ['title' => 'Uploaded Files']);
@@ -16,5 +23,37 @@ class UserController extends Controller {
     public function showProfile() {
 
         $this->render('dashboard/profile', ['title' => 'Profile']);
+    }
+
+    public function upload(Request $request) {
+
+        Validation::make()->rules($this->uploadRules())->data($request->getParams())->files($request->getFiles())->validate();
+
+        if (!Error::getInstance()->hasError()) {
+            File::Do()->saveUploadedFile($request->getFiles()['file'], $request->getParams());
+            Redirect::to('/')->go();
+        } else
+            Redirect::to('/')->data(['errors' => Error::getInstance()])->go();
+
+    }
+
+    public function uploadRules() {
+        return [
+          'title' => [
+              'alphanumeric',
+              ['minLen', 4],
+              ['maxLen', 24],
+          ],
+          'fileName' => [
+              'alphabetic'
+          ],
+          'price' => [
+              'numeric',
+          ],
+          'file' => [
+              ['size', 10 * 1000 * 1000],
+              ['type', 'jpg', 'pdf', 'zip'],
+          ]
+        ];
     }
 }
