0ed48ac860a03083b5f6fa95a7c46027d15c0fdf
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-11 14:37:25 +0430

    auth class initialised

--- app/core/Auth.php
+++ app/core/Auth.php
@@ -2,7 +2,35 @@
 
 namespace app\core;
 
-class Auth
-{
+use app\models\User;
 
+class Auth {
+    private static ?Auth $instance = null;
+    private array $user = [];
+
+    private function __construct() {
+        $this->check();
+    }
+
+    public static function getInstance() {
+        if (self::$instance == null)
+            self::$instance = new Auth();
+
+        return self::$instance;
+    }
+
+    public function check() {
+        if (!isset($_COOKIE['user_id']))
+            return;
+
+        $this->user = User::Do()->getUserById($_COOKIE['user_id']);
+    }
+
+    public function getName() {
+        return $this->user['firstname'] . ' ' . $this->user['lastname'];
+    }
+
+    public function getId() {
+        return $this->user['id'];
+    }
 }
