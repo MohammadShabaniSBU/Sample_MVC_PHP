1dfc68f6bddee4605178b6cde31c760a3a0cf6b9
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-11 13:45:34 +0430

    redirect class intialised

--- app/core/Redirect.php
+++ app/core/Redirect.php
@@ -2,7 +2,29 @@
 
 namespace app\core;
 
-class Redirect
-{
+class Redirect {
+    private static ?Redirect $instance = null;
+    private string $target;
 
+    private function __construct(string $target) {
+        $this->target = $target;
+    }
+
+    public static function to(string $target) {
+        if (self::$instance == null)
+            self::$instance = new Redirect($target);
+
+        return self::$instance;
+    }
+
+    public function data(array $data) {
+
+        foreach ($data as $key => $value)
+            Session::add($key, $value);
+        return $this;
+    }
+
+    public function go() {
+        header("Location: {$this->target}");
+    }
 }
