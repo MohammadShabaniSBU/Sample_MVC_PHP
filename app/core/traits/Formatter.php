f451ac43ab6776761035359c9c92d230396d8669
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-11 13:50:58 +0430

    formmater trait initialised

--- app/core/traits/Formatter.php
+++ app/core/traits/Formatter.php
@@ -2,9 +2,9 @@
 
 namespace app\core\traits;
 
-trait ForamtArray {
+trait Formatter {
 
-    private function format(array $targets, string $prefix = '', string $starter = '', string $finisher = '', string $separator = ',') {
+    protected function format(array $targets, string $prefix = '', string $starter = '', string $finisher = '', string $separator = ', ') {
         $string = $starter;
 
         foreach ($targets as $target) {
@@ -15,12 +15,22 @@ trait ForamtArray {
         return preg_replace("/{$separator}$/", $finisher, $string);
     }
 
-    private function formatEquation($targets) {
+    protected function formatEquation($targets) {
         $string = '';
 
         foreach ($targets as $target)
             $string .= "$target=:$target,";
 
         return substr($string, 0, strlen($string) - 1);
+    }
+
+    protected function formatSize(int $size) {
+
+        if ($size > 1000 * 1000)
+            return $size / (1000 * 1000) . ' Mb';
+        else if ($size > 1000)
+            return $size / 1000 . ' Kb';
+        else
+            return $size . ' b';
     }
 }
