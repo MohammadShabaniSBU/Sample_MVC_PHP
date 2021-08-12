e2d3d077f8fbf1263639655d4e75b7f5bd26340f
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-12 17:00:06 +0430

    logout button completed

--- view/dashboard/components/headbar.php
+++ view/dashboard/components/headbar.php
@@ -1,11 +1,17 @@
+<!-- Logout Modal -->
+<?php include \app\core\App::$root . "/view/modals/logoutModal.php"; ?>
+
 <div class="d-flex justify-content-between align-items-center p-2">
     <div class="fw-bold fs-4">
-        mohammad shabani
+        <?php echo \app\core\Auth::getInstance()->getName(); ?>
     </div>
     <div>
-        <a href="" data-bs-toggle="tooltip" data-bs-placement="top" title="Logout">
-            <i class="bi bi-x-circle fs-3 text-danger"></i>
+        <a href="/" data-bs-toggle="tooltip" data-bs-placement="top" title="Home">
+            <i class="bi bi-house-fill fs-3 text-info"></i>
         </a>
+        <button class="bg-light border-0 p-0" type="button" data-bs-toggle="modal" data-bs-target="#logoutModal" title="Logout">
+            <i class="bi bi-x-circle fs-3 text-danger"></i>
+        </button>
     </div>
 </div>
 <hr>
