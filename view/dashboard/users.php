1ac26718eae2fd2a20d23f55ef873407c83836ca
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-13 16:12:53 +0430

    activation modal and table rows completed

--- view/dashboard/users.php
+++ view/dashboard/users.php
@@ -1,4 +1,3 @@
-
 <!-- Edit Modal -->
 <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModal" aria-hidden="true">
     <div class="modal-dialog">
@@ -49,8 +48,47 @@
     </div>
 </div>
 
+<!-- Activation Modals -->
+<?php $users = \app\models\User::Do()->getAllUsers(); foreach ($users as $user) { ?>
+
+<div class="modal fade" id="activationModal<?php echo $user['id'];?>" tabindex="-1" aria-labelledby="activationModal<?php echo $user['id'];?>" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="activationModal<?php echo $user['id'];?>"><?php echo $user['firstname'] . ' ' . $user['lastname'];?></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div>
+                    Are you sure to <?php echo $user['status'] ? 'disactive' : 'active'; ?> <span class="fw-bold"><?php echo $user['firstname'] . ' ' . $user['lastname'];?></span>?
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <form action="/changeUserStatus/<?php echo $user['id']; ?>" method="post">
+                        <input type="hidden" value="<?php echo $user['status'] ? 'disactive' : 'active'; ?>" name="action">
+                    <input type="submit" class="btn btn-<?php echo $user['status'] ? 'danger' : 'success'; ?>" value="<?php echo $user['status'] ? 'Disactive' : 'Active'; ?>">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 
+<?php } ?>
+
+
+<?php
+    function getColorType(string $type) {
+        switch ($type) {
+            case 'admin':
+                return 'danger';
+            case 'normal':
+                return 'primary';
+
+        }
+    }
+?>
 
 <div class="row m-0">
     <div class="d-flex justify-content-center align-items-center h-100-vh">
@@ -65,38 +103,32 @@
                             <thead>
                             <tr>
                                 <th scope="col">#</th>
-                                <th scope="col">Title</th>
-                                <th scope="col">Uploaded Time</th>
-                                <th scope="col">Price</th>
-                                <th scope="col">Count of Download</th>
-                                <th scope="col">Download</th>
-                                <th scope="col">Edit</th>
-                                <th scope="col">Delete</th>
+                                <th scope="col">Firstname</th>
+                                <th scope="col">Lastname</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Type</th>
+                                <th scope="col">Status</th>
                             </tr>
                             </thead>
                             <tbody>
+                            <?php $i = 1; foreach ($users as $user) { ?>
                             <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                                <td>25</td>
+                                <th scope="row"><?php echo $i++;?></th>
+                                <td><?php echo $user['firstname']; ?></td>
+                                <td><?php echo $user['lastname']; ?></td>
+                                <td><?php echo $user['email']; ?></td>
                                 <td>
-                                    <a href="">
-                                        <i class="bi bi-cloud-download fs-5 text-success"></i>
-                                    </a>
+                                    <div class="bg-<?php echo getColorType($user['type'])?> rounded text-light">
+                                        <?php echo $user['type']; ?>
+                                    </div>
                                 </td>
                                 <td>
-                                    <a href=""  data-bs-toggle="modal" data-bs-target="#editModal">
-                                        <i class="bi bi-pencil fs-5 text-primary"></i>
-                                    </a>
-                                </td>
-                                <td>
-                                    <a href="" data-bs-toggle="modal" data-bs-target="#deleteModal">
-                                        <i class="bi bi-trash fs-5 text-danger"></i>
-                                    </a>
+                                    <button class="btn btn-<?php echo $user['status'] ? 'success' : 'danger'; ?> py-0" data-bs-toggle="modal" data-bs-target="#activationModal<?php echo $user['id'];?>">
+                                        <?php echo $user['status'] ? 'Active' : 'Disactive'; ?>
+                                    </button>
                                 </td>
                             </tr>
+                            <?php } ?>
                             </tbody>
                         </table>
                     </div>
