cb01aa174d1a12bb44a6c7a39fc12afb2491eded
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-13 16:16:12 +0430

    changeUserStatus method added

--- app/controller/AdminController.php
+++ app/controller/AdminController.php
@@ -3,23 +3,26 @@
 namespace app\controller;
 
 
+use app\core\Request;
+
 class AdminController extends Controller {
 
     public function showRequests() {
-
 
         $this->render('dashboard/requests', ['title' => 'Requested Files']);
     }
 
     public function showUsers() {
 
-
         $this->render('dashboard/users', ['title' => 'Users Management']);
     }
 
     public function showSettings() {
 
-
         $this->render('dashboard/settings', ['title' => 'Settings']);
+    }
+
+    public function changeUserStatus(Request $request, $id) {
+        echo $id;
     }
 }
