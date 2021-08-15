a6df803979a5261294e81f31c1c646513aa554ba
Mohammad Shabani <mohamad.darghah@gmail.com>
2021-08-15 10:54:33 +0430

    auth middleware refactored to be able to handle messages

--- app/middlewares/Auth.php
+++ app/middlewares/Auth.php
@@ -3,15 +3,27 @@
 namespace app\middlewares;
 
 use app\core\Auth as User;
+use app\models\User as UserModel;
 use app\core\Redirect;
 
 class Auth implements MiddlewareInterface {
+    private static array $message = [];
 
     public function check() : bool {
-        return User::getInstance()->isLogin();
+        if (User::getInstance()->isLogin()) {
+            if (!UserModel::Do()->isBan(User::getInstance()->getId())) {
+                return true;
+            } else {
+                self::$message['errorMessage'] = "Your account is banned.";
+                return false;
+            }
+        } else {
+            self::$message['warningMessage'] = "You must be a user";
+            return false;
+        }
     }
 
     public function redirect() {
-        Redirect::to('/signIn')->go();
+        Redirect::to('/signIn')->data(self::$message)->go();
     }
 }
